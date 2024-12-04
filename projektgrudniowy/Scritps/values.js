7e6c5d38d3cd1eccf215ee7c2a6ff059ee62edef
LenaMoz <145196770+LenaMoz@users.noreply.github.com>
2024-12-04 19:20:06 +0100

    Delete projektgrudniowy directory

--- projektgrudniowy/Scritps/values.js
+++
@@ -1,4 +0,0 @@
-const menu = document.querySelector("#menu");
-const menuContainer = document.querySelector("#menuContainer");
-const menuClose = document.querySelector("#menu--close");
-// ---------------------------------------------------------------
